f9abce201821b31bf74e2fa7bdf915ee92bcb727
apple00juice <alessio999@hotmail.ch>
2019-02-08 22:14:08 +0100

    Added BottomNavigationView for MainActivity and possibility to change tabs
    new Java classes and their layouts
    MainActivity
    GroupsFragment
    HomeFragment
    NotificationFragment
    ProfileFragment

---
+++ app/src/main/java/com/unnamed/studnetz/main/MainActivity.java
@@ -0,0 +1,64 @@
+package com.unnamed.studnetz.main;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.unnamed.studnetz.R;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        replaceFragment(new HomeFragment());
+    }
+
+    private boolean replaceFragment(Fragment fragment){
+        if(fragment != null) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.main_fragment_container, fragment);
+            fragmentTransaction.commitNow();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()){
+
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+
+            case R.id.navigation_groups:
+                fragment = new GroupsFragment();
+                break;
+
+            case R.id.navigation_notifications:
+                fragment = new NotificationsFragment();
+                break;
+
+            case R.id.navigation_profile:
+                fragment = new ProfileFragment();
+                break;
+        }
+
+        return replaceFragment(fragment);
+    }
+}
