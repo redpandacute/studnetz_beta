304d7b300df3e484de4fb1b9b43ee1fd33fe9239
apple00juice <alessio999@hotmail.ch>
2019-02-12 11:54:48 +0100

    Fragment change in MainActivity not added to backStack anymore because it wasn't useful and didn't felt right
    new SearchFilterFragment, set search parameters
    Layout incomplete
    new SearchResultFragment, showing search results
    Layout missing
    
    Interface added to communicate between HomeFragment and MainActivity

--- app/src/main/java/com/unnamed/studnetz/main/MainActivity.java
+++ app/src/main/java/com/unnamed/studnetz/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.unnamed.studnetz.main;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,6 +9,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.unnamed.studnetz.R;
@@ -15,13 +18,12 @@ import com.unnamed.studnetz.main.fragments.GroupsFragment;
 import com.unnamed.studnetz.main.fragments.HomeFragment;
 import com.unnamed.studnetz.main.fragments.NotificationsFragment;
 import com.unnamed.studnetz.main.fragments.ProfileFragment;
+import com.unnamed.studnetz.main.fragments.SearchFilterFragment;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, FragmentManager.OnBackStackChangedListener {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, HomeFragment.OnFragmentInteractionListener {
 
     BottomNavigationView bottomNavigation;
-
-    final static String MY_FRAGMENT_TAG = "active_fragment";
-
+    ImageButton SearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +33,20 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         bottomNavigation = findViewById(R.id.navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(this);
 
-        replaceFragment(new HomeFragment());
 
-        getSupportFragmentManager().addOnBackStackChangedListener(this);
+
+        replaceFragment(new HomeFragment(),false);
+
+
 
     }
 
-    private boolean replaceFragment(Fragment fragment){
+    private boolean replaceFragment(Fragment fragment, boolean addToBackStack){
         if(fragment != null) {
             FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.main_fragment_container, fragment,MY_FRAGMENT_TAG);
+            fragmentTransaction.replace(R.id.main_fragment_container, fragment);
 
-            if(getSupportFragmentManager().findFragmentByTag(MY_FRAGMENT_TAG) != null) {
+            if(addToBackStack){
                 fragmentTransaction.addToBackStack(null);
             }
 
@@ -53,62 +57,35 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     }
 
     // Sets active Fragment if the selected menuItem of the BottomNavigationBar is changed
-    // Also makes sure that the active fragment is not reset if it is equal to the menuItem
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         Fragment fragment = null;
-        Fragment activeFragment = getSupportFragmentManager().findFragmentByTag(MY_FRAGMENT_TAG);
 
         switch (menuItem.getItemId()){
 
             case R.id.navigation_home:
-                if(activeFragment instanceof HomeFragment)
-                    return true;
                 fragment = new HomeFragment();
                 break;
 
 
             case R.id.navigation_groups:
-                if(activeFragment instanceof GroupsFragment)
-                    return true;
                 fragment = new GroupsFragment();
                 break;
 
             case R.id.navigation_notifications:
-                if(activeFragment instanceof NotificationsFragment)
-                    return true;
                 fragment = new NotificationsFragment();
                 break;
 
             case R.id.navigation_profile:
-                if(activeFragment instanceof ProfileFragment)
-                    return true;
                 fragment = new ProfileFragment();
                 break;
         }
 
-        return replaceFragment(fragment);
+        return replaceFragment(fragment, false);
     }
 
-
-    // Sets selected Item of bottomNavigationBar if back button is pressed
-    // that it matches the active fragment
     @Override
-    public void onBackStackChanged() {
-        Fragment activeFragment = getSupportFragmentManager().findFragmentByTag(MY_FRAGMENT_TAG);
-
-        if (activeFragment instanceof GroupsFragment) {
-            if(bottomNavigation.getSelectedItemId() != R.id.navigation_groups)
-            bottomNavigation.setSelectedItemId(R.id.navigation_groups);
-        } else if (activeFragment instanceof NotificationsFragment) {
-            if(bottomNavigation.getSelectedItemId() != R.id.navigation_notifications)
-            bottomNavigation.setSelectedItemId(R.id.navigation_notifications);
-        } else if (activeFragment instanceof ProfileFragment) {
-            if(bottomNavigation.getSelectedItemId() != R.id.navigation_profile)
-            bottomNavigation.setSelectedItemId(R.id.navigation_profile);
-        } else {
-            if(bottomNavigation.getSelectedItemId() != R.id.navigation_home)
-            bottomNavigation.setSelectedItemId(R.id.navigation_home);
-        }
+    public void onHomeFragmentInteraction() {
+        replaceFragment(new SearchFilterFragment(), true);
     }
 }
--- app/src/main/java/com/unnamed/studnetz/main/fragments/HomeFragment.java
+++ app/src/main/java/com/unnamed/studnetz/main/fragments/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.unnamed.studnetz.main.fragments;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,14 +9,51 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.unnamed.studnetz.R;
 
 public class HomeFragment extends Fragment {
 
+
+
+    public interface OnFragmentInteractionListener{
+        void onHomeFragmentInteraction();
+    }
+
+    private OnFragmentInteractionListener mListener;
+    ImageButton mSearchButton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, null);
+         final View mView = inflater.inflate(R.layout.fragment_home, null);
+
+        mSearchButton = mView.findViewById(R.id.fragment_home_searchbutton);
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onHomeFragmentInteraction();
+            }
+        });
+
+        return mView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if(context instanceof OnFragmentInteractionListener){
+            mListener = (OnFragmentInteractionListener) context;
+        }else{
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
     }
 }
