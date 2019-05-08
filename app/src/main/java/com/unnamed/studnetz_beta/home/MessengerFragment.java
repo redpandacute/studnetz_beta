26f4e03f4adb4e2ce949b5adfb85bc2426396d4c
apple00juice <alessio999@hotmail.ch>
2019-05-08 21:04:31 +0200

    Added Messenger_Fragment, Profile_Fragments
    removed fragment transaction Animation

---
+++ app/src/main/java/com/unnamed/studnetz_beta/home/MessengerFragment.java
@@ -0,0 +1,59 @@
+package com.unnamed.studnetz_beta.home;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.unnamed.studnetz_beta.R;
+
+public class MessengerFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public MessengerFragment() {
+        // Required empty public constructor
+    }
+
+    static MessengerFragment newInstance() {
+        MessengerFragment fragment = new MessengerFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_messenger, container, false);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    interface OnFragmentInteractionListener {}
+}
---
+++ app/src/main/java/com/unnamed/studnetz_beta/home/ProfileFragment.java
@@ -0,0 +1,59 @@
+package com.unnamed.studnetz_beta.home;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.unnamed.studnetz_beta.R;
+
+public class ProfileFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    static ProfileFragment newInstance() {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    interface OnFragmentInteractionListener {}
+}
