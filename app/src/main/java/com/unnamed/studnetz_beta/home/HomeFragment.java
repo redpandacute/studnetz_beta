261b0019b5ae9296b38e7bd4c50c8604ae7b6aea
apple00juice <alessio999@hotmail.ch>
2019-05-09 15:45:18 +0200

    Added Toolbar to Home Fragment

--- app/src/main/java/com/unnamed/studnetz_beta/home/HomeFragment.java
+++ app/src/main/java/com/unnamed/studnetz_beta/home/HomeFragment.java
@@ -1,17 +1,27 @@
 package com.unnamed.studnetz_beta.home;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.unnamed.studnetz_beta.R;
+
+import java.io.IOException;
 
 public class HomeFragment extends Fragment {
 
@@ -37,7 +47,45 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        MaterialToolbar mToolBar = view.findViewById(R.id.material_toolbar_home);
+
+        try {
+            AppCompatActivity activity = ((AppCompatActivity) getActivity());
+            activity.setSupportActionBar(mToolBar);
+
+            mToolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.action_profile:
+                            // TODO: Change Fragment to Profile_Fragment
+                            break;
+
+                        case R.id.action_settings:
+                            // TODO: Create Settings Fragment
+                            break;
+                    }
+                    return false;
+                }
+            });
+
+        }catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
+        inflater.inflate(R.menu.menu_toolbar_main,menu);
     }
 
     @Override
